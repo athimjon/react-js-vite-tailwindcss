cc0ad2249049782da9d64ae1e81b2f870d9bef3d
athimjon <tonym5one@gmail.com>
2025-12-04 16:33:34 +0500

    Statistics ✔

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 			<img
 				src={logo}
 				alt='Logo Not found!'
-				className='w-[70px] h-[50px] cursor-pointer'
+				className='w-[60px] h-[55px] cursor-pointer'
 			/>
 			{/* NAVIGATION LINKS */}
 			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,3 +1,21 @@
+export const statistics = [
+	{
+		id: 1,
+		title: 'Foydalanuvchi Faol',
+		value: '9300+',
+	},
+	{
+		id: 2,
+		title: 'Kompaniya Homiyligida',
+		value: '100+',
+	},
+	{
+		id: 3,
+		title: 'Tranzaksiya',
+		value: '110M+',
+	},
+]
+
 export const navigationLinks = [
 	{
 		id: 'home',
